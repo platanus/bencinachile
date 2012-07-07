a5bd40753c4d51195f312f6a07155d301a45dcab
Juan Ignacio Donoso <jidonoso@gmail.com>
2012-07-06 22:29:30 -0400

    Prepares the app to retreive position

--- BencinaChile/MainPage.xaml.cs
+++ BencinaChile/MainPage.xaml.cs
@@ -10,15 +10,50 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using System.Device.Location;
+using Microsoft.Phone.Reactive;
 
 namespace BencinaChile
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        GeoCoordinateWatcher gcw;
+        
         // Constructor
         public MainPage()
         {
             InitializeComponent();
+
+
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (gcw == null)
+                gcw = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+
+            gcw.Start();
+
+            gcw.StatusChanged += (sender, ev) =>
+            {
+                if (ev.Status == GeoPositionStatus.Ready)
+                {
+                    System.Diagnostics.Debug.WriteLine(gcw.Position.Location.Latitude.ToString() + ", " + gcw.Position.Location.Longitude.ToString());
+
+                    // Stop the gps
+                    gcw.Stop();
+                }
+            };
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+            if (gcw != null)
+                gcw.Stop();
         }
     }
 }
