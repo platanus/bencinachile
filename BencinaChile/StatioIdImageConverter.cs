a149168dd25d4c1a49e516f303c8e98a30c0d8ce
Juan Ignacio Donoso <jidonoso@gmail.com>
2012-07-13 09:27:36 -0400

    Adds ImageConverter to show the brand logos

---
+++ BencinaChile/StatioIdImageConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+
+namespace BencinaChile
+{
+    public class StationIdImageConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (DesignerProperties.IsInDesignTool)
+                return "/Icons/5.png";
+            return String.Format("/Icons/{0}.png", value);
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
