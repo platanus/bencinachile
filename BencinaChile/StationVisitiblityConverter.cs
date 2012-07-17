c5d6c1560ffc575a74b0717efe4704761274e079
Juan Ignacio Donoso <jidonoso@gmail.com>
2012-07-17 19:10:55 -0400

    Adds new logos, changes name

---
+++ BencinaChile/StationVisitiblityConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+using System.ComponentModel;
+using System.Windows;
+
+namespace BencinaChile
+{
+    public class StationVisitiblityConverter : IValueConverter
+    {
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (DesignerProperties.IsInDesignTool)
+                return Visibility.Visible;
+            return (value != null) ? Visibility.Visible : Visibility.Collapsed;
+            
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
